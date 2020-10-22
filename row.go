0561857d52564ba1a93d3e89e9821d7400590201
Gabriel <gabriel.smith01@gmail.com>
2020-10-21 21:09:01 -0400

    added cli flags

--- row.go
+++ row.go
@@ -4,10 +4,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"time"
 
 	"github.com/urfave/cli/v2"
 )
+
+// type Rowing struct {
+// 	date     time.Time
+// 	distance int64
+// 	pace     int64
+// }
+
+func duration(ts string) {
+	t, err := time.Parse(time.RFC3339, ts)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(t)
+
+	fmt.Println(t.Format("15:04:05.000"))
+
+	h, m, s := t.Clock()
+	ms := t.Nanosecond() / int(time.Millisecond)
+	fmt.Printf("%02d:%02d:%02d.%03d\n", h, m, s, ms)
+	fmt.Printf("%d", ms)
+}
+
+func fileExists() bool {
+	userprofile := os.Getenv("USERPROFILE")
+	info, err := os.Stat(path.Join(userprofile, "rowing.json"))
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
 
 func main() {
 	var date string
@@ -41,7 +72,18 @@ func main() {
 			},
 		},
 		Action: func(c *cli.Context) error {
+			// look for existing rowing.json file
+			// if not create a new one
+
+			// add entry to bottom of the file
 			fmt.Printf("Distance: %x, Pace: %s, Date: %s\n", distance, pace, date)
+
+			if fileExists() {
+				fmt.Printf("file exist\n")
+			} else {
+				fmt.Printf("file doesn't exist exists\n")
+			}
+
 			return nil
 		},
 	}
