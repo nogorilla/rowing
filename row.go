a67d4f0b53f9deb846a908f84e90d6a4b8e1f5a4
Gabriel <gabriel.smith01@gmail.com>
2020-11-01 15:37:48 -0500

    changed function names; added logging for time parsing

--- row.go
+++ row.go
@@ -16,19 +16,24 @@ import (
 // 	pace     int64
 // }
 
-func duration(ts string) {
-	t, err := time.Parse(time.RFC3339, ts)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(t)
+func calcLength(ts string) {
+	t1, err := time.Parse("15:04:05", ts)
+	t2, err := time.Parse("15:04:05", "00:00:00")
 
-	fmt.Println(t.Format("15:04:05.000"))
+	fmt.Println(err, t1.Sub(t2))
+	fmt.Println("hour", t1.Hour())
+	fmt.Println("minute", t1.Minute())
+	fmt.Println("second", t1.Second())
+}
 
-	h, m, s := t.Clock()
-	ms := t.Nanosecond() / int(time.Millisecond)
-	fmt.Printf("%02d:%02d:%02d.%03d\n", h, m, s, ms)
-	fmt.Printf("%d", ms)
+func calcPace(pace string) {
+	paceTime, err := time.Parse("4:05.0", pace)
+	zeroTime, err := time.Parse("4:05.0", "0:00.0")
+	fmt.Println(err, paceTime.Sub(zeroTime))
+
+	fmt.Println("hour", paceTime.Hour())
+	fmt.Println("minute", paceTime.Minute())
+	fmt.Println("second", paceTime.Second())
 }
 
 func fileExists() bool {
@@ -43,6 +48,7 @@ func fileExists() bool {
 func main() {
 	var date string
 	var distance int64
+	var length string
 	var pace string
 
 	app := &cli.App{
@@ -58,17 +64,24 @@ func main() {
 			},
 			&cli.Int64Flag{
 				Name:        "distance",
-				Usage:       "Distance",
+				Usage:       "Distance travelled",
 				Required:    true,
 				Aliases:     []string{"d"},
 				Destination: &distance,
 			},
 			&cli.StringFlag{
 				Name:        "pace",
-				Usage:       "Pace",
+				Usage:       "Pace per 500 meters",
 				Aliases:     []string{"p"},
 				Required:    true,
 				Destination: &pace,
+			},
+			&cli.StringFlag{
+				Name:        "length",
+				Usage:       "Time spent rowing",
+				Aliases:     []string{"l"},
+				Required:    true,
+				Destination: &length,
 			},
 		},
 		Action: func(c *cli.Context) error {
@@ -77,6 +90,9 @@ func main() {
 
 			// add entry to bottom of the file
 			fmt.Printf("Distance: %x, Pace: %s, Date: %s\n", distance, pace, date)
+
+			calcLength(length)
+			calcPace(pace)
 
 			if fileExists() {
 				fmt.Printf("file exist\n")
